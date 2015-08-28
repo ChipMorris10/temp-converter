b23da5cd3005518c39450d5f582f6ed0806cb6b0
ChipMorris10 <goldcomac@mac.com>
2015-08-27 19:53:54 -0600

    submitting old file

--- main.js
+++ main.js
@@ -3,17 +3,15 @@
 var temp = prompt("What is the temperature you want to convert?");
 var celciusFahrenheit = prompt("Do you want to convert that to Celsius or Fahrenheit?");
 
-// If the user entered Fahrenheit
+// If the user entered Fahrenheit. .toFixed rounds down to 1 digit.
 if (celciusFahrenheit === "Fahrenheit") {
 
   returnTemp = ((5/9) * (temp - 32)).toFixed(1);
   alert("The temperature is " + returnTemp);
 }
 
-// If the user entered Celsius
+// If the user entered Celsius. .toFixed rounds down to 1 digit.
 else {
   returnTemp = ((temp * (9/5) + 32)).toFixed(1);
   alert ("The temperature is " + returnTemp);
 }
-
-// temp * (9/5) + 32;
